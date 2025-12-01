401cd5c03100ccf34405b793b618a20481c8c062
Ishalana <laneech2@gmail.com>
2025-12-01 11:40:50 -0600

    Made the database a little easier to read.

--- FinalProject/DataModel.cs
+++ FinalProject/DataModel.cs
@@ -1,21 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using Entities;
+
 public class Zoo : DbContext
 {
   public DbSet<Penguin>? Penguins { get; set; }
   public DbSet<Dragon>? Dragons { get; set; }
   public DbSet<Enclosure> Enclosures { get; set; }
 
-  protected override void OnConfiguring(
-    DbContextOptionsBuilder optionsBuilder)
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
-    string path = Path.Combine(
-      Environment.CurrentDirectory, "Zoo.db");
-
+    string path = Path.Combine(Environment.CurrentDirectory, "Zoo.db");
     string connection = $"Filename={path}";
 
     Console.WriteLine($"Connection: {connection}");
-
     optionsBuilder.UseSqlite(connection);
   }
 }
